029e9b2f4aabcecb535386954f51bb84351f31ae
Product Builder <builder@bringyour.com>
2025-11-26 21:37:38 -0800

    2025.11.26-794218300

--- glog.go
+++ glog.go
@@ -113,8 +113,8 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/urnetwork/glog/internal/logsink"
-	"github.com/urnetwork/glog/internal/stackdump"
+	"github.com/urnetwork/glog/v2025/internal/logsink"
+	"github.com/urnetwork/glog/v2025/internal/stackdump"
 )
 
 var timeNow = time.Now // Stubbed out for testing.
